c0c080d18ccb9d252fd67925af37e2b665f4b907
Khushbu Pareek <khushbu@MacBook-Pro.local>
2020-03-17 23:11:24 -0700

    corrected directory structure

---
+++ build/precache-manifest.ebea076fb3d6f367e0072a4a26026898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e46416d2890213a9e4d155dd41d93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922b6a8fb09cff13a110",
+    "url": "/static/css/main.e778fb4e.chunk.css"
+  },
+  {
+    "revision": "5ce9abbc789a03683604",
+    "url": "/static/js/2.fe79c2ff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.fe79c2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922b6a8fb09cff13a110",
+    "url": "/static/js/main.718e0cb5.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
